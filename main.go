4a63f4da10e5ea1df25768f45dad273ed030a629
DemonKingSwarn <rockingswarn@gmail.com>
2022-06-22 17:21:30 +0530

    variable, types and constant

--- main.go
+++ main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+const LoginToken string = "dghhfjghjdghj" // Public
+
 func main() {
-	fmt.Println("Hello There")
+	// fmt.Println("Hello There")
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+	
+	var smallFloatVal float64 = 255.4558278236786479
+	fmt.Println(smallFloatVal)
+	fmt.Printf("Variable is of type: %T \n", smallFloatVal)
+
+	// default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type
+
+	var website = "demonkingswarn.is-a.dev"
+	fmt.Println(website)
+
+	// no var style
+
+	numberOfUser := 300000.0
+	fmt.Println(numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
 }
